d3f7685a60ae759ba55f737ad12a7ff9c90c5fdd
tatiana-leonova <lts221@yandex.ru>
2020-04-19 17:41:46 +0300

    Правки в слайдер на главной, удаление лишних пробелов в коде, правки в соц сети в футере

--- source/js/nav.js
+++ source/js/nav.js
@@ -12,3 +12,18 @@ var navMain = document.querySelector('.main-nav');
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+  // var navMain = document.querySelector('.main-nav');
+  // var navToggle = document.querySelector('.main-nav__toggle');
+
+  // navMain.classList.remove('main-nav--nojs');
+
+  // navToggle.addEventListener('click', function() {
+  //   if (navMain.classList.contains('main-nav--opened')) {
+  //     navMain.classList.remove('main-nav--opened');
+  //     navMain.classList.add('main-nav--closed');
+  //   } else {
+  //     navMain.classList.add('main-nav--opened');
+  //     navMain.classList.remove('main-nav--closed');
+  //   }
+  // });
