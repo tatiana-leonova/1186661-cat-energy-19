078f618548050b16f4849f665370e533ceab3d83
tatiana-leonova <lts221@yandex.ru>
2020-04-21 01:27:08 +0300

    Добавление адаптивной графики

--- source/js/nav.js
+++ source/js/nav.js
@@ -4,26 +4,11 @@ var navMain = document.querySelector('.main-nav');
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
+    if (navMain.classList.contains('main-nav--opened')) {
       navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+    } else {
+      navMain.classList.add('main-nav--opened');
+      navMain.classList.remove('main-nav--closed');
     }
   });
-
-  // var navMain = document.querySelector('.main-nav');
-  // var navToggle = document.querySelector('.main-nav__toggle');
-
-  // navMain.classList.remove('main-nav--nojs');
-
-  // navToggle.addEventListener('click', function() {
-  //   if (navMain.classList.contains('main-nav--opened')) {
-  //     navMain.classList.remove('main-nav--opened');
-  //     navMain.classList.add('main-nav--closed');
-  //   } else {
-  //     navMain.classList.add('main-nav--opened');
-  //     navMain.classList.remove('main-nav--closed');
-  //   }
-  // });
