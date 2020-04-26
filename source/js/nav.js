838f1a6063698916da852f1a21f7b137f7c1ebe4
tatiana-leonova <lts221@yandex.ru>
2020-04-26 19:26:01 +0300

    Fix problem with git

---
+++ source/js/nav.js
@@ -0,0 +1,26 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  function changeState () {
+    navMain.classList.toggle('main-nav--closed');
+    navMain.classList.toggle('main-nav--opened');
+  }
+
+  navToggle.onclick = function () {
+  changeState();
+  };
+
+  changeState();
+
+
+  // navToggle.addEventListener('click', function() {
+  //   if (navMain.classList.contains('main-nav--opened')) {
+  //     navMain.classList.remove('main-nav--opened');
+  //     navMain.classList.add('main-nav--closed');
+  //   } else {
+  //     navMain.classList.add('main-nav--opened');
+  //     navMain.classList.remove('main-nav--closed');
+  //   }
+  // });
